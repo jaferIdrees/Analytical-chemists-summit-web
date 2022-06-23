1558233d3b80a3c9af69b1f1e7a580f083d4c472
Jafer Yousef <jaferll@gmail.com>
2022-06-23 14:39:57 +0300

    Change authors images

--- scripts/script.js
+++ scripts/script.js
@@ -13,37 +13,37 @@
       */
 const authors = [{
   name: 'Gene Hall',
-  authImg: 'speakers/speaker_01.png',
+  authImg: 'speakers/speaker_01_.png',
   authText1: 'Professor, Analytical Chemist, Rutgers University',
   authText2: 'Gene is an analytical chemist who specializes in ICPMS, FT-IR, Raman, MALDI, Q-TOF, and XRF Laboratory. Dr. Hall earned his BS degree from Tusculum College and his PhD from Virginia Polytechnic.',
 },
 {
   name: 'Renã Robinson',
-  authImg: 'speakers/speaker_03.png',
+  authImg: 'speakers/speaker_02_.png',
   authText1: 'Associate Professor of Chemistry Vanderbilt University',
   authText2: 'She has a nationally and internationally recognized research program and is a leader in the field of proteomics for her work in aging, Alzheimer’s disease, and applications relevant to human health and disparities.',
 },
 {
   name: 'Siqi Sun',
-  authImg: 'speakers/speaker_02.png',
+  authImg: 'speakers/speaker_03_.png',
   authText1: 'Application Lab Manager, Analytik Jena US',
   authText2: 'She led the application research and technical support for the North American market. She is the voting member of American Society for Testing and Materials (ASTM) Committee D02 on Petroleum Products, Liquid Fuels, and Lubricants and D19 on water.',
 },
 {
   name: 'Toby Blackburn',
-  authImg: 'speakers/speaker_04.png',
+  authImg: 'speakers/speaker_04_.png',
   authText1: 'Head of Business Development and Strategy, Emerald Cloud Lab',
   authText2: 'Toby Blackburn serves as the head of business development and strategy at Emerald Cloud Lab (ECL), a physical laboratory which scientists can access remotely via the Internet that allows them to run, analyze, and interpret experiments without setting foot in the lab.',
 },
 {
   name: 'Brian Frezza',
-  authImg: 'speakers/speaker_05.png',
+  authImg: 'speakers/speaker_05_.png',
   authText1: 'Co-Founder and Co-Chief Executive Officer Emerald Cloud Lab',
   authText2: 'Brian is a Hollerith and Wolfram Innovator award-winner who has received over four hundred combined citations for published articles in the Journal of the American Chemical Society (JACS) and Chemical Physics. He has also served as a peer reviewer for both JACS and Langmuir, and guest-lectured at CMU, TSRI, and Stanford.',
 },
 {
   name: 'SohYeong Noh',
-  authImg: 'speakers/speaker_06.png',
+  authImg: 'speakers/speaker_06_.png',
   authText1: 'Berkman Professor of Enterpreneurial Legal Studies at Harvard Law School',
   authText2: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
 }];
@@ -84,17 +84,22 @@ window.addEventListener('load', () => {
   for (let i = 0; i < authors.length; i += 1) {
     addAuthor(i);
   }
+  document.getElementById('menuBtn').click();
   document.getElementById('seeMore').click();
   document.getElementById('seeMore').click();
 });
 
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
   const mainMenu = document.querySelector('.mainMenu');
-  console.log(mainMenu.style.backgroundColor);
   if (window.scrollY > 200) {
     mainMenu.style.backgroundColor = 'white';
   } else {
     mainMenu.style.backgroundColor = 'transparent';
   }
+});
+
+// change drop down menu backgroundColor
+
+document.getElementById('menuBtn').addEventListener('click', () => {
+  document.querySelector('.collapsed').style.backgroundColor = 'white';
 });
